a12f4f26ed1e8ef7682aa9aa5607e4fa0064b65b
Praneeth Karnena <praneeth.codes@gmail.com>
2019-07-22 23:27:48 +0530

    updated GitHub repo URL

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description='Python client for FBI crime data API of the United States',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/PraneethKarnena/pyfbi',
+    url='https://github.com/PraneethKarnena/pyfbicrime',
     packages=setuptools.find_packages(),
     classifiers=[
         'DEVELOPMENT STATUS :: 5 - PRODUCTION/STABLE',
